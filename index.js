ff20cfbbf21aded0ef9bfd4afd12bfccf3732f04
Monday <sobaata@gmail.com>
2021-07-22 22:06:34 -0400

    Completed.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching (array, string) {
+    return array.filter(x => x.toLowerCase() === string.toLowerCase()) 
+}
+
+function fuzzyMatch (array, string){
+    return array.filter (x => x.startsWith(string))
+}
+
+function matchName (array, string){
+    return array.filter (x => x.name===string)
+}
